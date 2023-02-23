7f84a4befeca07e30f74494718d5f200419aeec3
Emeson12345678910 <124007178+Emeson12345678910@users.noreply.github.com>
2023-02-23 17:12:35 -0600

    Codigo semana#5
    .

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingresa primer valor");
+String Valor = Console.ReadLine();
+int A = Convert.ToInt32(Valor);
+
+Console.WriteLine("Ingresa Segundo valor");
+String Valor2 = Console.ReadLine();
+int Valor_2 = Convert.ToInt32(Valor2);
+
+for (int indice = A; indice < Valor_2; indice++)
+{
+    if (indice % 2 == 0) Console.WriteLine(indice);
+}
